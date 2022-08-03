b20670f705873a3d8086e5fc4fd9ccc673050e49
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-08-03 14:00:49 -0700

    vignette effect on video

---
+++ src/components/Comments/index.jsx
@@ -0,0 +1,14 @@
+const Comments = () => {
+  return (
+    <div>
+      <ul>
+        <li>comments</li>
+        <li>comments</li>
+        <li>comments</li>
+        <li>comments</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Comments;
