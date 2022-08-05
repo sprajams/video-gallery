9982283caa68c958b2334eddb1f425db0fe36000
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-08-04 17:54:36 -0700

    minor styling changes

--- src/components/Comments/index.jsx
+++ src/components/Comments/index.jsx
@@ -1,15 +1,30 @@
+import styles from "./styles.module.scss";
+
 const Comments = ({ setActiveComments }) => {
   const onClickClose = () => {
     setActiveComments(false);
   };
   return (
-    <div>
-      <button onClick={onClickClose}>X</button>
-      <ul>
-        <li>comments</li>
-        <li>comments</li>
-        <li>comments</li>
-        <li>comments</li>
+    <div className={styles.outer}>
+      <button onClick={onClickClose} className={styles.closeBtn}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fillRule="evenodd"
+            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+      <ul className={styles.wrap}>
+        <li className={styles.comment}>comments</li>
+        <li className={styles.comment}>comments</li>
+        <li className={styles.comment}>comments</li>
+        <li className={styles.comment}>comments</li>
       </ul>
     </div>
   );
