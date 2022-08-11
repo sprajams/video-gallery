a9a139019a72a8b65840170a4aa041c09fac026f
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-08-11 13:18:44 -0700

    follow button functionality

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
 import Grid from "../Grid";
 import styles from "./styles.module.scss";
+import clsx from "clsx";
 
 const Home = ({ dataSnippet, setActiveGallery, setInitialIndex }) => {
+  const [isFollowing, setIsFollowing] = useState(false);
+
+  const onClickFollow = () => {
+    setIsFollowing(!isFollowing);
+  };
+
   return (
     <div className={styles.outer}>
       <h2 className={styles.username}>@Kona</h2>
@@ -29,10 +37,53 @@ const Home = ({ dataSnippet, setActiveGallery, setInitialIndex }) => {
               </div>
             </div>
           </div>
-          <button className={styles.btnFollow}>Follow</button>
+          <div className={styles.btnContainer}>
+            <button
+              className={clsx(
+                styles.btn,
+                styles.btnFollow,
+                isFollowing && styles.active
+              )}
+              onClick={onClickFollow}
+            >
+              {isFollowing ? "Following" : "Follow"}
+            </button>
+            <div className={styles.smallBtnWrap}>
+              <button className={clsx(styles.btn, styles.btnIcon)}>
+                {/* down icon */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </button>
+              {isFollowing && (
+                <button className={clsx(styles.btn, styles.btnIcon)}>
+                  {/* alert bell icon */}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z" />
+                  </svg>
+                </button>
+              )}
+            </div>
+          </div>
           <div className={styles.profileBio}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo optio
-            libero voluptatum. Deleniti id debitis eos.
+            Just a small brown girl. Livin' in a lonely world. She took the
+            midnight train going anywhere.
+            <br />
+            Follow for more content!
           </div>
         </div>
         <Grid
