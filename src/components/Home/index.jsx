da8ba02556d07aa7c943c83c7ef3fb16b35dab5c
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-08-11 16:22:58 -0700

    fix icon on mobile browsers

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -60,30 +60,34 @@ const Home = ({ dataSnippet, setActiveGallery, setInitialIndex }) => {
                 onClick={onClickOptions}
               >
                 {/* down icon */}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </button>
-              {isFollowing && (
-                <button className={clsx(styles.btn, styles.btnIcon)}>
-                  {/* alert bell icon */}
+                <div className={styles.icon}>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="h-5 w-5"
                     viewBox="0 0 20 20"
                     fill="currentColor"
                   >
-                    <path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z" />
+                    <path
+                      fillRule="evenodd"
+                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    />
                   </svg>
+                </div>
+              </button>
+              {isFollowing && (
+                <button className={clsx(styles.btn, styles.btnIcon)}>
+                  {/* alert bell icon */}
+                  <div className={styles.icon}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z" />
+                    </svg>
+                  </div>
                 </button>
               )}
             </div>
